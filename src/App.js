818af5f080ebd3c88f5728766a1c268618db9b23
axer4 <somersault97@gmail.com>
2022-04-20 21:56:52 +0300

    finish with slider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import s from './App.module.css'
 import FeaturesMugs from "./components/featuresMugs/FeaturesMugs";
 import MoreProducts from './components/moreProducts/moreProducts';
 import PremiumOffer from './components/premiumOffer/PremiumOffer';
+import SimpleSlider from './components/Slider/Slider';
 const  App = () => {
   return (
     <div className={s.app}>
       <FeaturesMugs/>
       <MoreProducts/>
       <PremiumOffer/>
+      <SimpleSlider/>
     </div>
   );
 }
---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import s from './Slider.module.css';
+import productsImages from '../../images/moreProducts';
+const {
+    summerCup,
+    winterStyleMug,
+    blackTeaCup,
+    ceramicTea,
+    espressoCup,
+    essentialsMug,
+    noHandleCup,
+    pinkPremium,
+    redLoveCup
+} = productsImages;
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+          }
+        },]
+    };
+    return (
+      <div className={s.slider}>
+        <h2> Sliders Item</h2>
+        <Slider {...settings}>
+        <div className={s.products_item}>
+                        <img
+                        src={redLoveCup}
+                        alt='redLoveCup'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>Red Love Cup</h2>
+                        <div className={s.inline_block}>
+                            <p className={s.mugs_item_redPrice}>$25.00</p>
+                            <p className={s.mugs_item_oldPrice}>$ 37.00 USD</p>
+                            </div>
+                    </div>
+                    <div className={s.products_item}>
+                        <img
+                        src={blackTeaCup}
+                        alt='blackTeaCup'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>Black Tea Cup</h2>
+                        <div className={s.inline_block}>
+                            <p className={s.mugs_item_redPrice}>$15.00</p>
+                            <p className={s.mugs_item_oldPrice}>$ 29.00 USD</p>
+                            </div>
+                    </div>
+                    <div className={s.products_item}>
+                        <img
+                        src={essentialsMug}
+                        alt='essentialMug'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>B{`&`}W Essentials Mug</h2>
+                        <p className={s.mugs_item_price}>$ 19.00 USD</p>
+                    </div>
+                    <div className={s.products_item}>
+                        <img
+                        src={winterStyleMug}
+                        alt='winterStyleMug'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>Winter Style Mug</h2>
+                        <p className={s.mugs_item_price}>$ 25.00 USD</p>
+                    </div>
+                    <div className={s.products_item}>
+                        <img
+                        src={ceramicTea}
+                        alt='ceramicTea'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>Ceramic Tea</h2>
+                        <p className={s.mugs_item_price}>$ 46.00 USD</p>
+                    </div>
+                    <div className={s.products_item}>
+                        <img
+                        src={noHandleCup}
+                        alt='noHandleCup'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>No Handle Bar Cup</h2>
+                        <p className={s.mugs_item_price}>$ 34.00 USD</p>
+                    </div>
+                    <div className={s.products_item}>
+                        <img
+                        src={espressoCup}
+                        alt='espressoCup'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>Espresso Cup by Mugs.co</h2>
+                        <p className={s.mugs_item_price}>$ 25.00 USD</p>
+                    </div>
+                    <div className={s.products_item}>
+                        <img
+                        src={pinkPremium}
+                        alt='pinkPremium'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>Pink Premium Ceramic</h2>
+                        <p className={s.mugs_item_price}>$ 99.00 USD</p>
+                    </div>
+                    <div className={s.products_item}>
+                        <img
+                        src={summerCup}
+                        alt='pinkPremium'
+                        className={s.products_image}
+                        />
+                        <h2 className={s.products_heading}>Summer Designer Cup</h2>
+                        <p className={s.mugs_item_price}>$ 29.00 USD</p>
+                    </div>
+        </Slider>
+      </div>
+    );
+  }
+}
